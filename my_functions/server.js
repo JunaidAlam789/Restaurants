dba147f3c3534797a0b8a847b2d3b0f2825c9ee8
Muhammad Junaid Alam <89206859+JunaidAlam789@users.noreply.github.com>
2022-08-10 20:04:48 +0500

    Add files via upload

--- my_functions/server.js
+++ my_functions/server.js
@@ -1,3 +1,4 @@
+exports.handler = async function (event, context) {
 const path = require("path")
 const express = require("express")
 const app = express()
@@ -9,75 +10,38 @@ app.use(cors());
 var restaurant;
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const { async } = require("regenerator-runtime");
+const { handler } = require("./serverbackup");
+console.log("hello")
 
-exports.handler = async function (event, context) {
   const uri = "mongodb+srv://testdb:testdb123@cluster0.2yklpfd.mongodb.net/?retryWrites=true&w=majority";
       const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
       // try{
         const database = client.db('sample_restaurants');
         const restaurants = database.collection('restaurants');
-    try{
-  let name = event.path.split("/").pop()
-  if (name === "cuisine") {
-    return {
-      statusCode: 200,
-      headers: { "Cache-Control": "max-age=10" },
-      body: JSON.stringify({
-        American: "American",
-         Pakistani: "Pakistani",
-         Indian: "Indian",
-         Italian: "Italian"
-       })
-    }
-  } else if (name === "American") {
+       // console.log(restaurants)
+
+       // exports.handler = async function (event, context) {    
+  //let name = event.path.split("/").pop()
+   
          
                   
          // perform actions on the collection object
+         //async function run(restaurant){
          const query = { cuisine :  'American' };
          //const restaurant =collection.findOne(query).then(console.log("hello")).catch();
-         const cursor= restaurants.find(query);
-        var restlist=[];
-        await cursor.forEach(rest => {
-        restlist.push(rest.name);   })
-         res.json(
-              //message: 'Hello World'
-             { restlist}
-          );
-          console.log(restlist,"list")
-        
-          
-    // } //try
-    //  catch{err=>console.log(err)}
-    //      finally{
-    //          client.close() 
-    //      }
-  
-     
-  
-  
-
-    
-    return {
-      statusCode: 200,
-      headers: { "Cache-Control": "max-age=10" },
-      body: JSON.stringify({ American: "American",
-      Pakistani: "Pakistani",
-      Indian: "Indian",
-      Italian: "Italian" })
-    }
-  } else {
-    return {
-      statusCode: 404,
-      headers: { "Cache-Control": "max-age=10" },
-      body: JSON.stringify({ message: "Cuisine not found." })
-    }
-  }
-
-}// try
- catch{err=>console.log(err)}
-    finally{
-         client.close() 
-     }
-  
-  
+        restaurant=await restaurants.findOne(query);
+         console.log(restaurant);
+         //}
+   
+console.log("closing hello");
+client.close() 
+return {
+  statusCode: 200,
+  headers: { "Cache-Control": "max-age=10" },
+  body: JSON.stringify({ message: "Hello World"})
 }
+     
+      } 
+  
+
+
