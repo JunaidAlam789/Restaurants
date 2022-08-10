fb4ac033993ee823a8d17f291e669a5fd13114a2
Muhammad Junaid Alam <89206859+JunaidAlam789@users.noreply.github.com>
2022-08-10 16:57:59 +0500

    Add files via upload

--- my_functions/server.js
+++ my_functions/server.js
@@ -1,4 +1,20 @@
 exports.handler = async function (event, context) {
+  const path = require("path")
+  const express = require("express")
+  const app = express()
+  const cors = require('cors');
+  
+  app.use(express.static(path.join(__dirname, "public")))
+  
+  app.use(cors());
+  var restaurant;
+  const { MongoClient, ServerApiVersion } = require('mongodb');
+  const { async } = require("regenerator-runtime");
+  const uri = "mongodb+srv://testdb:testdb123@cluster0.2yklpfd.mongodb.net/?retryWrites=true&w=majority";
+  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+  client.close;
+
+
   let name = event.path.split("/").pop()
   if (name === "cuisine") {
     return {
