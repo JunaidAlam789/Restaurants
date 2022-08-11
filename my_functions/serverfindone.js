4ac5f15eea9da436511c9a62701c1409d45e58a4
Muhammad Junaid Alam <89206859+JunaidAlam789@users.noreply.github.com>
2022-08-11 17:50:15 +0500

    Add files via upload

---
+++ my_functions/serverfindone.js
@@ -0,0 +1,88 @@
+exports.handler = async function (event, context) {
+    let name = event.path.split("/").pop()
+    if (name === "Indian") {
+        return {
+          statusCode: 200,
+          headers: { "Cache-Control": "max-age=10" },
+          body: JSON.stringify({ name: "Indian" })
+        }
+      } else    if (name === "Pakistani") {
+//const dotenv = require("dotenv")
+//dotenv.config()
+const mongodb = require("mongodb")
+const express = require("express")
+const app = express()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://testdb:testdb123@cluster0.2yklpfd.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+//process.env.CONNECTIONSTRING
+console.log(name,"hello1");
+client.connect( err=> {
+    async function run (){
+ // app.get("/", async (req, res) => {
+    console.log("hello2");
+    try {
+       // const db = client.db("sample_restaurants");
+     // const rest = await db.collection("restaurants").find({ cuisine: "Pakistani" }).toArray()
+     const database = client.db('sample_restaurants');
+     const restaurants = database.collection('restaurants');
+    // const query = { name: `${restname}` };
+     const query = { name: "Subway" };
+     const rest = await restaurants.findOne(query);
+    // rest= ["hotel1","hotel2","hotel3"];
+      
+        console.log(rest);
+
+      // since this method returns the matched document, not a cursor, print it directly
+      //console.log(restaurant);
+      //res.json(restaurant);
+        
+        return {
+            statusCode: 200,
+            headers: { "Cache-Control": "max-age=10" },
+            body: JSON.stringify(rest)
+          }
+          
+        
+       /* else {
+        //res.json("You do not currently have any restaurant in this cuisine collection.")
+        
+        return {
+            statusCode: 200,
+            headers: { "Cache-Control": "max-age=10" },
+            body: JSON.stringify({ message: "Restaurant not found." })
+          }
+      
+      } */
+    } catch (err) {
+      console.log(err)
+      //res.json("Try again later.")
+      
+      return {
+        statusCode: 404,
+        headers: { "Cache-Control": "max-age=10" },
+        body: JSON.stringify({ message: "error." })
+      }
+      
+
+      
+    }
+    //finally{
+     // client.close();
+      
+    //}
+    
+ // }) //app get closing
+  } // async anonymous function closing
+  run()
+  //app.listen(process.env.PORT || 3000)
+});
+//client.close();
+    }
+}
+ //app.listen(process.env.PORT || 3000)
+
+
+
+
