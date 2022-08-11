07f49fca352486030ec5fcac40f1c5f6964c3e1f
Muhammad Junaid Alam <89206859+JunaidAlam789@users.noreply.github.com>
2022-08-11 23:33:45 +0500

    Add files via upload

--- my_functions/serverfindone.js
+++ my_functions/serverfindone.js
@@ -1,4 +1,5 @@
 exports.handler = async function (event, context) {
+ // exports.handler = async function (event, context) {
     //let name = event.path.split("/").pop()
     
 //const dotenv = require("dotenv")
@@ -26,14 +27,15 @@ client.connect( async (err)=> {
     //subway={"_id":"5eb3d668b31de5d588f42fa9","address":{"building":"2508","coord":[-73.9727279,40.7928619],"street":"Broadway","zipcode":"10025"},"borough":"Manhattan","cuisine":"Sandwiches","grades":[{"date":"2014-05-30T00:00:00.000Z","grade":"A","score":5},{"date":"2013-05-24T00:00:00.000Z","grade":"A","score":12},{"date":"2012-05-30T00:00:00.000Z","grade":"A","score":7},{"date":"2012-01-18T00:00:00.000Z","grade":"A","score":5},{"date":"2011-08-11T00:00:00.000Z","grade":"A","score":13}],"name":"Subway","restaurant_id":"40539022"}
        
     console.log(subway);
+    
+    
+    })
     client.close();
     return   ({
       statusCode: 200,
       headers: { "Cache-Control": "max-age=10" },
       body: JSON.stringify(subway)
     })
-    })
-      
         
     
   //}) //app get closing
