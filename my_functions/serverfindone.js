a143501575cd798533115504216f8acd6ba0e2e8
Muhammad Junaid Alam <89206859+JunaidAlam789@users.noreply.github.com>
2022-08-11 18:05:46 +0500

    Add files via upload

--- my_functions/serverfindone.js
+++ my_functions/serverfindone.js
@@ -23,11 +23,12 @@ client.connect( err=> {
     console.log("hello2");
     try {
      
-     const database = client.db('sample_restaurants');
-     const restaurants = database.collection('restaurants');
+    // const database = client.db('sample_restaurants');
+    // const restaurants = database.collection('restaurants');
     // const query = { name: `${restname}` };
      //const query = { name: "Subway" };
      //const rest = await restaurants.findOne(query);
+     const restaurants={"_id":"5eb3d668b31de5d588f42fa9","address":{"building":"2508","coord":[-73.9727279,40.7928619],"street":"Broadway","zipcode":"10025"},"borough":"Manhattan","cuisine":"Sandwiches","grades":[{"date":"2014-05-30T00:00:00.000Z","grade":"A","score":5},{"date":"2013-05-24T00:00:00.000Z","grade":"A","score":12},{"date":"2012-05-30T00:00:00.000Z","grade":"A","score":7},{"date":"2012-01-18T00:00:00.000Z","grade":"A","score":5},{"date":"2011-08-11T00:00:00.000Z","grade":"A","score":13}],"name":"Subway","restaurant_id":"40539022"}
        
     console.log(restaurants);
 
