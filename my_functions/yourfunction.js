ec24e4658465b34bfee69a2ade997c2f2d140f33
Muhammad Junaid Alam <89206859+JunaidAlam789@users.noreply.github.com>
2022-08-12 16:50:27 +0500

    Add files via upload

--- my_functions/yourfunction.js
+++ my_functions/yourfunction.js
@@ -1,8 +1,10 @@
-const mongodb = require("mongodb")
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://testdb:testdb123@cluster0.2yklpfd.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 exports.handler = async function (event, context) {
-  const client = await mongodb.connect(uri, { useUnifiedTopology: true })
+  //const client = await mongodb.connect(uri, { useUnifiedTopology: true })
+  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
   const db = client.db()
 
   try {
