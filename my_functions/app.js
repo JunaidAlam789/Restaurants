f0de8a4c98b8e60013fda02b0f755a5a34ea03ab
Muhammad Junaid Alam <89206859+JunaidAlam789@users.noreply.github.com>
2022-08-12 17:23:56 +0500

    Add files via upload

--- my_functions/app.js
+++ my_functions/app.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const app = express()
 const client = require("./db.js")
-const db = client.db()
+const db = client.db("sample_restaurants")
 
 app.get("/", async (req, res) => {
   try {
--- my_functions/db.js
+++ my_functions/db.js
@@ -1,7 +1,7 @@
 const dotenv = require("dotenv")
 dotenv.config()
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const CONNECTIONSTRING = "mongodb+srv://testdb:testdb123@cluster0.2yklpfd.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://testdb:testdb123@cluster0.2yklpfd.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 //mongodb.connect(CONNECTIONSTRING, { useUnifiedTopology: true }, function (err, client) {
   module.exports = client
